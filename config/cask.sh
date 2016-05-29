f1c56bceb7f67b03a3ef2ae5b0ced9e4d62f5e89
ItsJonQ <itsjonq@gmail.com>
2016-05-29 13:24:58 -0400

    Adds cask app installer

---
+++ config/cask.sh
@@ -0,0 +1,25 @@
+# cask.sh
+# Install apps via Brew
+# @itsjonq
+
+echo "Starting brew cask install…"
+
+# Install apps!
+brew cask install 1password
+brew cask install dropbox
+brew cask install firefox
+brew cask install flux
+brew cask install google-chrome
+brew cask install karabiner
+brew cask install licecap
+brew cask install karabiner
+brew cask install onyx
+brwe cask install screenhero
+brew cask install sketch
+brew cask install spotify
+brew cask install sublime-text
+brew cask install telegram
+brew cask install vlc
+
+echo "Brew cask installation complete!"
+
