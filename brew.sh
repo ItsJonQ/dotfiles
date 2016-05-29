f5e84938fbbd92c27a63ea7007c4b0deeb6d03be
ItsJonQ <itsjonq@gmail.com>
2016-05-28 23:36:45 -0400

    Added osx and brew setups

---
+++ brew.sh
@@ -0,0 +1,50 @@
+# brew.sh
+# Install all the homebrew things
+# @itsjonq
+# ============================================================================
+# Inspired by Mathias!
+# https://github.com/mathiasbynens/dotfiles/blob/master/brew.sh
+# ============================================================================
+
+echo "Starting brew setup"
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# Install GNU core utilities (those that come with OS X are outdated).
+# Don’t forget to add `$(brew --prefix coreutils)/libexec/gnubin` to `$PATH`.
+brew install coreutils
+# ln -s /usr/local/bin/gsha256sum /usr/local/bin/sha256sum
+
+# Install some other useful utilities like `sponge`.
+brew install moreutils
+# Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install findutils
+
+# Install more recent versions of some OS X tools.
+brew install vim --override-system-vi
+brew install homebrew/dupes/grep
+brew install homebrew/dupes/openssh
+brew install homebrew/dupes/screen
+brew install homebrew/php/php56 --with-gmp
+
+# Install brew things
+brew install ffmpeg
+brew install git
+brew install googler
+brew install imagemagick --with-webp
+brew install macvim
+brew install node
+brew install pow
+brew install rbenv
+brew install the_silver_searcher
+brew install tree
+brew install x264
+
+# Remove outdated versions from the cellar.
+brew cleanup
+
+echo "Brew setup complete!"
---
+++ setup.sh
@@ -0,0 +1,5 @@
+# Ask for the administrator password upfront
+sudo -v
+
+sh brew.sh
+sh .osx
