c31a56e8c88477acd0e4dfaf4792e3ed1407c0d0
ItsJonQ <itsjonq@gmail.com>
2016-05-29 13:14:13 -0400

    Updated ruby things

--- config/ruby.sh
+++ config/ruby.sh
@@ -3,13 +3,19 @@
 
 echo "Starting gem setup…"
 
+# Update to latest gems
+gem update --system
+
 # Install global ruby things
-gem install bundler
-gem install jekyll
-gem install powder
-# gem install rails
+gem install bundler --no-rdoc --no-ri
+gem install jekyll --no-rdoc --no-ri
+gem install powder --no-rdoc --no-ri
+gem install rails --no-rdoc --no-ri
 # gem install sass
-gem install s3_website
+gem install s3_website --no-rdoc --no-ri
+
+# Cleanup gems
+gem cleanup
 
 echo "Gem setup complete!"
 
