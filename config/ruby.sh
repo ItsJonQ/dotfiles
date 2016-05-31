ecd8fe3ea308e904ec36e7751687663788204d8a
ItsJonQ <itsjonq@gmail.com>
2016-05-31 12:49:46 -0400

    Install sass gem in ruby sh

--- config/ruby.sh
+++ config/ruby.sh
@@ -11,7 +11,7 @@ gem install bundler --no-rdoc --no-ri
 gem install jekyll --no-rdoc --no-ri
 gem install powder --no-rdoc --no-ri
 gem install rails --no-rdoc --no-ri
-# gem install sass
+gem install sass --no-rdoc --no-r
 gem install s3_website --no-rdoc --no-ri
 
 # Cleanup gems
