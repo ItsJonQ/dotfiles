d7646aa4adf53cc09769d625527a239704e783dd
ItsJonQ <itsjonq@gmail.com>
2016-06-07 23:00:42 -0400

    Adds quiet sync and hush

---
+++ sync.sh
@@ -0,0 +1,14 @@
+# sync.sh
+# Author: Q <itsjonq@gmail.com>
+# Source: https://github.com/ItsJonQ/dotfiles
+# =============================================================================
+# Quietly sync dotfiles
+
+for file in $HOME/dotfiles/.*; do
+  if [[ -f $file ]]; then
+    F=`basename "$file"`
+    rsync -ah --no-perms "$F" "$HOME"/"$F"
+  fi
+  echo "Dotfiles updated!"
+done
+
