4375ec6e6d20df0c47438f261da2439eff941638
ItsJonQ <itsjonq@gmail.com>
2016-12-08 09:44:40 -0500

    Adds app installer

---
+++ apps.sh
@@ -0,0 +1,10 @@
+# apps.sh
+# Author: Q <itsjonq@gmail.com>
+# Source: https://github.com/ItsJonQ/dotfiles
+# =============================================================================
+# Installs all the apps!
+
+# Ask for the administrator password upfront
+sudo -v
+
+sh config/cask.sh
