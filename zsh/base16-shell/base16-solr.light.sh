07d04eae27b2292377e27207d0a1dc71e38822d7
ItsJonQ <itsjonq@gmail.com>
2017-03-14 12:33:24 -0400

    Updates light solr

--- zsh/base16-shell/base16-solr.light.sh
+++ zsh/base16-shell/base16-solr.light.sh
@@ -7,7 +7,7 @@ if [ "${TERM%%-*}" = 'linux' ]; then
     return 2>/dev/null || exit 0
 fi
 
-color00="07/36/42" # Base 00 - Black
+color00="fd/f6/e3" # Base 00 - Black
 color01="07/36/42" # Base 08 - Red
 color02="07/36/42" # Base 0B - Green
 color03="07/36/42" # Base 0A - Yellow
