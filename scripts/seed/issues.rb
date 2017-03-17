f6b6c675c9a8848b3fc3dba801efdef93d970261
ItsJonQ <itsjonq@gmail.com>
2017-03-17 09:39:04 -0400

    Adds seed-issues script

---
+++ scripts/seed/issues.rb
@@ -0,0 +1,20 @@
+#!/usr/bin/env ruby
+
+require "json"
+require "net/http"
+require "pp"
+
+uri = URI("https://api.github.com/search/repositories?q=seed+user:helpscout")
+response = JSON.parse(Net::HTTP.get(uri))
+
+repos = response["items"].inject({}) do |list, (repo)|
+  issues = repo["open_issues_count"]
+  if issues and issues > 0
+    list[repo["name"]] = issues
+  end
+  list
+end
+
+puts ""
+pp(repos)
+puts ""
