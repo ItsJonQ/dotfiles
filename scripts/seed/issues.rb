500e12ec83fcac8065b0ca83856f0aea2a325ac2
ItsJonQ <itsjonq@gmail.com>
2017-03-23 08:32:00 -0400

    Updates seed-issues script

--- scripts/seed/issues.rb
+++ scripts/seed/issues.rb
@@ -4,17 +4,22 @@ require "json"
 require "net/http"
 require "pp"
 
-uri = URI("https://api.github.com/search/repositories?q=seed+user:helpscout&per_page=200")
+uri = URI("https://api.github.com/search/repositories?q=seed+user:helpscout&per_page=300")
 response = JSON.parse(Net::HTTP.get(uri))
 
 repos = response["items"].inject({}) do |list, (repo)|
   issues = repo["open_issues_count"]
   if issues and issues > 0
-    list[repo["name"]] = issues
+    list[repo["name"]] = {
+      issues: issues,
+      url: repo["html_url"],
+    }
   end
   list
 end
 
 puts ""
-pp(repos)
+repos.each do |r|
+  puts r
+end
 puts ""
