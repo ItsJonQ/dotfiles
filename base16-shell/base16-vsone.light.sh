3e9456a50063c23678de17f89bd6247bd64317c7
ItsJonQ <itsjonq@gmail.com>
2017-06-29 22:18:55 -0400

    Update vsplus

--- base16-shell/base16-vsone.light.sh
+++ base16-shell/base16-vsone.light.sh
@@ -8,36 +8,36 @@ if [ "${TERM%%-*}" = 'linux' ]; then
 fi
 
 
-color00="1e/1e/1e" # Base 00 - Black
-color01="56/9c/d6" # Base 08 - Red
-color02="d0/87/70" # Base 0B - Green
-color03="ff/ff/ff" # Base 0A - Yellow
-color04="56/9c/d6" # Base 0D - Blue
-color05="56/9c/d6" # Base 0E - Magenta
-color06="56/9c/d6" # Base 0C - Cyan
-color07="dd/dd/dd" # Base 05 - White
+color00="ef/f1/f5" # Base 00 - Black
+color01="9e/60/b1" # Base 08 - Red
+color02="a6/7b/55" # Base 0B - Green
+color03="cc/cc/cc" # Base 0A - Yellow
+color04="4e/8c/bf" # Base 0D - Blue
+color05="4e/8c/bf" # Base 0E - Magenta
+color06="4e/8c/bf" # Base 0C - Cyan
+color07="4f/5b/66" # Base 05 - White
 
-color08="55/55/55" # Base 03 - Bright Black
+color08="a7/ad/ba" # Base 03 - Bright Black
 color09=$color01 # Base 08 - Bright Red
 color10=$color02 # Base 0B - Bright Green
 color11=$color03 # Base 0A - Bright Yellow
 color12=$color04 # Base 0D - Bright Blue
 color13=$color05 # Base 0E - Bright Magenta
 color14=$color06 # Base 0C - Bright Cyan
-color15="ff/ff/ff" # Base 07 - Bright White
-color16="ce/91/78" # Base 09
-color17="56/9c/d6" # Base 0F
-color18="25/25/26" # Base 01
-color19="33/33/33" # Base 02
-color20="bb/bb/bb" # Base 04
+color15="2c/32/3c" # Base 07 - Bright White
+color16="f0/c6/74" # Base 09
+color17="61/af/ef" # Base 0F
+color18="df/e1/e8" # Base 01
+color19="c0/c5/ce" # Base 02
+color20="65/73/7e" # Base 04
 
-color21="f3/f3/f3" # Base 06
+color21="2c/32/3c" # Base 06
 
 
-color_foreground="dd/dd/dd" # Base 05
-color_background="1e/1e/1e" # Base 00
+color_foreground="4f/5b/66" # Base 05
+color_background="ef/f1/f5" # Base 00
 
-color_cursor="dd/dd/dd" # Base 05
+color_cursor="4f/5b/66" # Base 05
 
 if [ -n "$TMUX" ]; then
   # tell tmux to pass the escape sequences through
@@ -86,13 +86,13 @@ printf $printf_template 21 $color21
 if [ -n "$ITERM_SESSION_ID" ]; then
   # iTerm2 proprietary escape codes
   
-  printf $printf_template_custom Pg dddddd # forground
-  printf $printf_template_custom Ph 1e1e1e # background
-  printf $printf_template_custom Pi dddddd # bold color
-  printf $printf_template_custom Pj 333333 # selection color
-  printf $printf_template_custom Pk dddddd # selected text color
-  printf $printf_template_custom Pl dddddd # cursor
-  printf $printf_template_custom Pm 1e1e1e # cursor text
+  printf $printf_template_custom Pg 4f5b66 # forground
+  printf $printf_template_custom Ph eff1f5 # background
+  printf $printf_template_custom Pi 4f5b66 # bold color
+  printf $printf_template_custom Pj c0c5ce # selection color
+  printf $printf_template_custom Pk 4f5b66 # selected text color
+  printf $printf_template_custom Pl 4f5b66 # cursor
+  printf $printf_template_custom Pm eff1f5 # cursor text
   
 else
   printf $printf_template_var 10 $color_foreground
