468485d15a59ed0d32e7ef71a257140eebf89149
ItsJonQ <itsjonq@gmail.com>
2017-11-20 12:19:38 -0500

    Update sombra

--- base16-shell/base16-sombra.light.sh
+++ base16-shell/base16-sombra.light.sh
@@ -10,10 +10,10 @@ fi
 
 color00="f9/f9/fb" # Base 00 - Black
 color01="8f/8f/92" # Base 08 - Red
-color02="cc/61/9e" # Base 0B - Green
+color02="cc/48/73" # Base 0B - Green
 color03="c6/c6/c9" # Base 0A - Yellow
-color04="cc/61/9e" # Base 0D - Blue
-color05="cc/61/9e" # Base 0E - Magenta
+color04="cc/48/73" # Base 0D - Blue
+color05="cc/48/73" # Base 0E - Magenta
 color06="c6/c6/c9" # Base 0C - Cyan
 color07="3b/47/52" # Base 05 - White
 
