365a27c5665f6e2a27d42fc211429d5dc286e2a1
ItsJonQ <itsjonq@gmail.com>
2018-03-16 18:33:30 -0400

    Add apps to node.sh

--- config/node.sh
+++ config/node.sh
@@ -24,12 +24,15 @@ npm install eslint-plugin-react -g
 npm install express -g
 npm install grunt-cli -g
 npm install gulp-cli -g
+npm install jest -g
 npm install jshint -g
 npm install mocha -g
 npm install np -g
 npm install npm-check-updates -g
 npm install prettier -g
 npm install pm2 -g
+npm install serverless -g
+npm install surge -g
 npm install webpack -g
 npm install @storybook/cli -g
 
