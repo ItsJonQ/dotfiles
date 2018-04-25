2c413646b054e6005541fa3ac7f32e9572325bf4
ItsJonQ <itsjonq@gmail.com>
2018-04-24 20:11:31 -0400

    Update base16-shell

--- base16-shell/base16-blk.dark.sh
+++ base16-shell/base16-blk.dark.sh
@@ -85,10 +85,10 @@ if [ -n "$ITERM_SESSION_ID" ]; then
   printf $printf_template_custom Pj 3c3c3c # selection color
   printf $printf_template_custom Pk ececec # selected text color
   
-  printf $printf_template_custom Pl ececec # cursor
+  printf $printf_template_custom Pl 50E3C2 # cursor
   
   
-  printf $printf_template_custom Pm 000000 # cursor text
+  printf $printf_template_custom Pm 000000 # cursor
   
 else
   printf $printf_template_var 10 $color_foreground
