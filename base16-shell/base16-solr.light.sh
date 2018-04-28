edaca4a8fab13b0b3402db1ce0235a54e8e8c0a2
ItsJonQ <itsjonq@gmail.com>
2018-04-28 07:50:03 -0400

    Update base16-shell

--- base16-shell/base16-solr.light.sh
+++ base16-shell/base16-solr.light.sh
@@ -9,13 +9,15 @@ fi
 
 
 color00="fd/f6/e3" # Base 00 - Black
-color01="07/36/42" # Base 08 - Red
-color02="07/36/42" # Base 0B - Green
-color03="07/36/42" # Base 0A - Yellow
-color04="07/36/42" # Base 0D - Blue
-color05="07/36/42" # Base 0E - Magenta
-color06="07/36/42" # Base 0C - Cyan
-color07="07/36/42" # Base 05 - White
+
+color01="cc/2d/61" # Base 08 - Red
+color02="5e/97/62" # Base 0B - Green
+color03="cc/92/50" # Base 0A - Yellow
+color04="49/7a/cc" # Base 0D - Blue
+color05="92/5b/b6" # Base 0E - Magenta
+color06="4e/5b/be" # Base 0C - Cyan
+color07="58/6e/75" # Base 05 - White
+
 
 color08="c8/c7/bb" # Base 03 - Bright Black
 color09=$color01 # Base 08 - Bright Red
@@ -25,15 +27,16 @@ color12=$color04 # Base 0D - Bright Blue
 color13=$color05 # Base 0E - Bright Magenta
 color14=$color06 # Base 0C - Bright Cyan
 color15="17/46/52" # Base 07 - Bright White
-color16="fd/f6/e3" # Base 09
-color17="fd/f6/e3" # Base 0F
+color16="f7/8c/6c" # Base 09
+color17="ff/5a/90" # Base 0F
 color18="f8/f0/dd" # Base 01
 color19="ee/e8/d6" # Base 02
 color20="65/7b/83" # Base 04
 
-color21="fd/f6/e3" # Base 06
+color21="17/46/52" # Base 06
 
 
+# vimLightText
 color_foreground="07/36/42" # Base 05
 color_background="fd/f6/e3" # Base 00
 
@@ -86,15 +89,13 @@ printf $printf_template 21 $color21
 if [ -n "$ITERM_SESSION_ID" ]; then
   # iTerm2 proprietary escape codes
   
-  
-  printf $printf_template_custom Pg 073642 # forground
+  printf $printf_template_custom Pg 586e75 # forground
   printf $printf_template_custom Ph fdf6e3 # background
-  
-  printf $printf_template_custom Pi 073642 # bold color
+  printf $printf_template_custom Pi 586e75 # bold color
   printf $printf_template_custom Pj eee8d6 # selection color
-  printf $printf_template_custom Pk 073642 # selected text color
+  printf $printf_template_custom Pk 586e75 # selected text color
   
-  printf $printf_template_custom Pl 073642 # cursor
+  printf $printf_template_custom Pl 586e75 # cursor
   
   
   printf $printf_template_custom Pm fdf6e3 # cursor text
