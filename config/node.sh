e3be01cff931fbab6cde5506e7b898d0a9b9d1c3
ItsJonQ <itsjonq@gmail.com>
2018-05-26 11:18:58 -0400

    Add spaceship prompt to node

--- config/node.sh
+++ config/node.sh
@@ -32,6 +32,7 @@ npm install npm-check-updates -g
 npm install prettier -g
 npm install pm2 -g
 npm install serverless -g
+npm install spaceship-prompt -g
 npm install surge -g
 npm install webpack -g
 npm install @storybook/cli -g
