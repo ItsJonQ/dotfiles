ffc621ca9eb5ec194fb8a2c5acd2763312e72cfc
ItsJonQ <itsjonq@gmail.com>
2019-01-31 22:48:25 -0500

    Update base16-shell

--- base16-shell/base16-term.dark.sh
+++ base16-shell/base16-term.dark.sh
@@ -10,10 +10,10 @@ fi
 color00="00/00/00" # Base 00 - Black
 color01="c4/1a/15" # Base 08 - Red
 color02="00/ca/35" # Base 0B - Green
-color03="31/84/95" # Base 0A - Yellow
+color03="f0/c6/74" # Base 0A - Yellow
 color04="32/8c/ff" # Base 0D - Blue
 color05="e4/00/e1" # Base 0E - Magenta
-color06="31/84/95" # Base 0C - Cyan
+color06="56/b6/c2" # Base 0C - Cyan
 color07="ec/ec/ec" # Base 05 - White
 color08="4c/4c/4c" # Base 03 - Bright Black
 color09=$color01 # Base 08 - Bright Red
