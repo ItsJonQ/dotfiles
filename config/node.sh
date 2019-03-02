343ed145a3b06baa8c1c86de274a71ed3503296b
Jon Q <hello@jonquach.com>
2019-03-01 21:19:58 -0500

    Update node.sh global installs

--- config/node.sh
+++ config/node.sh
@@ -14,26 +14,27 @@ npm update -g
 npm install babel -g
 npm install babel-eslint -g
 npm install babel-cli -g
-npm install bower -g
+# npm install bower -g
 npm install create-react-app -g
 npm install eslint -g
-npm install eslint-config-airbnb -g
-npm install eslint-plugin-import -g
-npm install eslint-plugin-jsx-a11y -g
-npm install eslint-plugin-react -g
-npm install express -g
+# npm install eslint-config-airbnb -g
+# npm install eslint-plugin-import -g
+# npm install eslint-plugin-jsx-a11y -g
+# npm install eslint-plugin-react -g
+# npm install express -g
 npm install grunt-cli -g
 npm install gulp-cli -g
 npm install jest -g
-npm install jshint -g
-npm install mocha -g
+# npm install jshint -g
+# npm install mocha -g
 npm install np -g
 npm install npm-check-updates -g
 npm install prettier -g
+npm install @prettier/plugin-php
 npm install pm2 -g
-npm install serverless -g
+# npm install serverless -g
 npm install spaceship-prompt -g
-npm install surge -g
+# npm install surge -g
 npm install webpack -g
 npm install @storybook/cli -g
 
