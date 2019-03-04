ea90f3b9ff470503bac6afe6f5851b408532b465
Jon Q <hello@jonquach.com>
2019-03-04 10:07:42 -0500

    Update vscode

--- symlink.sh
+++ symlink.sh
@@ -24,7 +24,7 @@ ln -sf $HOME/Drive/Config/.npmrc $HOME/.npmrc
 # ln -sf $HOME/Drive/Config/.aws $HOME
 ln -sf $HOME/Drive/Config/.zprofile $HOME/.zprofile
 
-ln -sf $HOME/dotfiles/.vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
-ln -sf $HOME/dotfiles/.vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
+ln -sf $HOME/Drive/Config/.vscode/keybindings.json $HOME/Library/Application\ Support/Code/User/keybindings.json
+ln -sf $HOME/Drive/Config/.vscode/settings.json $HOME/Library/Application\ Support/Code/User/settings.json
 
 echo Symlinked dotfiles.
