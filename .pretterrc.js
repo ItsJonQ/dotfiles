c55254132bb2a4eb6a346d9e6f7bef4def6528e4
Jon Q <hello@jonquach.com>
2019-10-14 19:10:22 -0400

    Replace prettierrc

---
+++ .pretterrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+  useTabs: true,
+  tabWidth: 4,
+  singleQuote: true,
+  trailingComma: 'es5',
+  bracketSpacing: true,
+  parenSpacing: true,
+  jsxBracketSameLine: false,
+  semi: true,
+  arrowParens: 'always',
+}
